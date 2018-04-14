dccde4230f02d4e21c75c875bb9fef9f22044283
Hubry <Hubry@users.noreply.github.com>
2018-04-14 23:54:20 +0200

    Bump version

--- src/main/java/hubry/landings/HappyLandings.java
+++ src/main/java/hubry/landings/HappyLandings.java
@@ -40,8 +40,8 @@ public class HappyLandings {
 
     public static final String MODID = "happylandings";
     public static final String NAME = "Happy Landings";
-    public static final String VERSION = "1.0";
-
+    public static final String VERSION = "1.1";
+    
     @SubscribeEvent
     public static void cancelElytraWallDamage(LivingAttackEvent event) {
         if (event.getEntity() instanceof EntityPlayer && Configs.isElytraDamageCancelled && event.getSource() == DamageSource.FLY_INTO_WALL) {
